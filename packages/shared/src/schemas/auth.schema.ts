6cbc44032bb6bf05f93951bf51abf069388dcfbe
Jabir7006 <www.roman1249@gmail.com>
2026-08-19 16:34:37 +0600

    feat(shared): add shared package with auth schemas

---
+++ packages/shared/src/schemas/auth.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z.string("Name is required"),
+  email: z.email("Valid Email is required"),
+  password: z
+    .string("Password is required")
+    .min(6, "Password must be at least 6 characters"),
+  role: z.enum(["MANAGER", "CHEF", "WAITER"]).default("CHEF"),
+});
+
+export type RegisterInput = z.infer<typeof registerSchema>;
